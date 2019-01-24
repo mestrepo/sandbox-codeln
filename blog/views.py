594d82a7f6370ae8646128b04394d9dfe6818a88
simon baaman suuk <simon.suuk@gmail.com>
2019-01-24 15:00:16 +0000

    added view to display list of posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    """a view to display the list of posts."""
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
