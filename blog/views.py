da2a1a5e280fe1e6c86a27e48654949bf696ac44
simon baaman suuk <simon.suuk@gmail.com>
2019-01-24 15:02:34 +0000

    added view to display details of a single post

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,16 @@ def post_list(request):
     """a view to display the list of posts."""
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    """a view to display details of a single post."""
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
