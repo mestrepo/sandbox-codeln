1ceffd72306e19be8e4a093397fd49b6ec27dfec
Simon Baaman Suuk <simon.suuk@gmail.com>
2019-01-27 01:12:26 +0000

    added pagination to split list of posts across several pages

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
 def post_list(request):
     """a view to display the list of posts."""
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 1)  # number of posts per page
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)  # deliver first page if page is not an integer
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)  # deliver last page if page is out of range
+
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
