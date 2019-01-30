0fa5d565038e5d571ca99eaae3929cf3a97a6127
Simon Baaman Suuk <simon.suuk@gmail.com>
2019-01-30 14:14:29 +0000

    added skeleton models for job and job_detail

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    pass
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        pass
+
+
+class JobDetails(models.Model):
+    pass
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        pass
