66261488d5a929936cbe9b05009d03cf9ba69b41
Simon Baaman Suuk <simon.suuk@gmail.com>
2019-02-11 00:13:16 +0000

    deleted migrations and renamed model fields

--- marketplace/models.py
+++ marketplace/models.py
@@ -69,7 +69,7 @@ class Job(models.Model):
     JOB_ROLE = (
         ('Full Stack Developer', 'Full Stack Developer'),
         ('Frontend Developer', 'Frontend Developer'),
-        ('backend_developer', 'Backend  Developer'),
+        ('Backend  Developer', 'Backend  Developer'),
         ('Android  Developer', 'Android  Developer'),
         ('Graphic Designer', 'Graphic Designer'),
         ('IOS Developer', 'IOS Developer'),
@@ -89,13 +89,13 @@ class Job(models.Model):
     # prevent multiple jobs from having the same slug for the same date
     slug = models.SlugField(max_length=250, unique_for_date='created')
     description = models.TextField()
-    job_role = models.CharField(max_length=30, choices=JOB_ROLE, default='full_stack_developer')
-    dev_experience = models.CharField(max_length=30, choices=DEV_EXPERIENCE, default='mid-level')
-    engagement_type = models.CharField(max_length=30, choices=ENGAGEMENT_TYPE, default='full_time')
+    job_role = models.CharField(max_length=30, choices=JOB_ROLE, default='Full Stack Developer')
+    dev_experience = models.CharField(max_length=30, choices=DEV_EXPERIENCE, default='Mid-Level')
+    engagement_type = models.CharField(max_length=30, choices=ENGAGEMENT_TYPE, default='Full-time')
     tech_stack = models.CharField(max_length=500)
     num_devs_wanted = models.IntegerField(default=1)
     # monthly remuneration for fulltime, contract etc. budget project for freelance
-    remuneration_in_dollars = models.CharField(max_length=45, help_text='in dollars ($)')
+    remuneration = models.CharField(max_length=45, help_text='in dollars ($)')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     position_filled = models.BooleanField(default=False)
